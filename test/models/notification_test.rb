20c5c56065a902afbade9401df31c44366fb2339
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-14 04:21:30 -0500

    add failing test

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "the false" do
+    assert false
+  end
 end
