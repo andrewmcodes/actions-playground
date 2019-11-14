38a880a0a4d8b78fef5bafc3fe0889cc50a3e4d4
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-14 04:46:38 -0500

    wip

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,21 @@
 require "test_helper"
 
+if ENV["SELENIUM_HOST"] == "selenium"
+  Capybara.javascript_driver = :selenium
+  Capybara.run_server = false
+
+  args = ["--no-default-browser-check", "--start-maximized", "--no-sandbox"]
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => args})
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :remote,
+      url: "http://#{ENV["SELENIUM_HOST"]}:#{ENV["SELENIUM_PORT"]}/wd/hub",
+      desired_capabilities: caps
+    )
+  end
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end
