1cd9712429fb6aa0a268b1ed51ed1e0aaa25db78
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-18 18:47:52 -0500

    try to use https

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,32 +1,38 @@
 require "test_helper"
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+if ENV["SELENIUM_REMOTE_URL"]
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  end
+
+  Capybara.register_driver :headless_chrome do |app|
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: {
+        args: %w[
+          headless disable-gpu no-sandbox
+          --window-size=1400,1400 --enable-features=NetworkService,NetworkServiceInProcess
+        ]
+      }
+    )
+
+    client = Selenium::WebDriver::Remote::Http::Default.new
+    client.timeout = 90
+
+    Capybara::Selenium::Driver.new app,
+      browser: :chrome,
+      url: ENV["SELENIUM_REMOTE_URL"],
+      desired_capabilities: capabilities,
+      http_client: client
+  end
+
+  Capybara.default_driver = :headless_chrome
+  Capybara.javascript_driver = :headless_chrome
 end
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: %w[
-        headless disable-gpu no-sandbox
-        --window-size=1400,1400 --enable-features=NetworkService,NetworkServiceInProcess
-      ]
-    }
-  )
-
-  client = Selenium::WebDriver::Remote::Http::Default.new
-  client.timeout = 90
-
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    url: ENV["SELENIUM_REMOTE_URL"],
-    desired_capabilities: capabilities,
-    http_client: client
-end
-
-Capybara.default_driver = :headless_chrome
-Capybara.javascript_driver = :headless_chrome
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :headless_chrome
+  if ENV["SELENIUM_REMOTE_URL"]
+    driven_by :headless_chrome
+  else
+    driven_by :selenium, using: :headless_chrome
+  end
 end
