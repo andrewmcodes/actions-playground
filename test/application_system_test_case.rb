2830322ffa1ab51041252aa15b071e7a051b6ba5
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-18 17:04:46 -0500

    test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,7 +7,7 @@ end
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: {
-      args: %w[headless enable-features=NetworkService,NetworkServiceInProcess]
+      args: %w[headless no-sandbox disable-dev-shm-usage enable-features=NetworkService,NetworkServiceInProcess]
     }
   )
 
@@ -22,5 +22,5 @@ Capybara.javascript_driver = :headless_chrome
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   system("which chromedriver")
-  driven_by :selenium, using: :headless_chrome
+  driven_by :headless_chrome
 end
